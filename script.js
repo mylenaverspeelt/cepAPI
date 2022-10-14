2be1d0d7993fa3e38103b68cc5479cb01f49267f
mylenaverspeelt <mylena49@hotmail.com>
2022-10-14 11:45:01 -0300

    final version

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// var consultaCEP = fetch("https://viacep.com.br/ws/01001250/json/")
-//   .then((response) => response.json())
-//   .then(r => {
-//     if (r.erro) {
-//       throw Error("Cep Inexistente");
-//     } else
-//       console.log(r);
-//   })
-//   .catch((erro) => console.log(erro))
-//   .finally(mensagem => console.log('Finalizado'));
-
-//   console.log(consultaCEP)
-
-// tem os dois then pq o primeiro é pra converter as infos que vem em bytes pra json, e o segundo é pra exibir pno console o retorno dessa response convertida em json.
-
-//essa primeira forma é utilizando vários then e catch pra ver se pega um erro. na segunda forma abaixo é utilizando funções assíncronas
-
 async function buscaEndereco(cep) {
   const mensagemErro = document.getElementById("erro");
   mensagemErro.innerHTML = "";
@@ -33,7 +16,7 @@ async function buscaEndereco(cep) {
     cidade.value = consultaCEPConvertida.localidade;
     logradouro.value = consultaCEPConvertida.logradouro;
     estado.value = consultaCEPConvertida.uf;
-    bairro.value = consultaCEPConvertida.bairro
+    bairro.value = consultaCEPConvertida.bairro;
 
     return consultaCEPConvertida;
   } catch (erro) {
@@ -42,8 +25,7 @@ async function buscaEndereco(cep) {
   }
 }
 
-//nessa segunda função,  o await é obrigatório pq é uma função assícrona
-
 var cep = document.getElementById("cep");
 
-cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));   //callback
+
